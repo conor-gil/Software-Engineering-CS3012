3a9b9fcecf26868b8a49a29e382c8f51f003ad38
conor-gil <33493170+conor-gil@users.noreply.github.com>
2018-09-19 12:22:14 +0100

    initial commit
    need to test function

---
+++ LCA.py
@@ -0,0 +1,35 @@
+
+#a binary tree node
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+# Finds the path from root node to given root of the tree.
+# Stores the path in a list path[], returns true if path
+# exists otherwise false
+def findPath(root, path, k):
+    # Baes Case
+    if root is None:
+        return False
+
+    # Store this node is path vector. The node will be
+    # removed if not in path from root to k
+    path.append(root.key)
+
+    # See if the k is same as root's key
+    if root.key == k:
+        return True
+
+    # Check if k is found in left or right sub-tree
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right != None and findPath(root.right, path, k))):
+        return True
+
+        # If not present in subtree rooted with root, remove
+    # root from path and return False
+
+    path.pop()
+    return False
---
+++ test_node.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from LCA import Node
+
+class TestNode(TestCase):
+
+    #need to add tests to test find path function
+
+    def test_demo(self):
+        self.fail()

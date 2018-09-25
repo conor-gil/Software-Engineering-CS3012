7f108b6883bc41f58320203a6b164b425db8cd09
conor-gil <conor.gilly11@gmail.com>
2018-09-25 11:35:36 +0100

    added tests, not great format

--- test_node.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from LCA import Node
-
-class TestNode(TestCase):
-
-    #need to add tests to test find path function
-
-    def test_demo(self):
-        self.fail()

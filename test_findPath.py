ed544f3359f3935edb28f4b8ce0231facc7561f1
conor-gil <conor.gilly11@gmail.com>
2018-10-02 19:39:48 +0100

    added lca function, added test with self

--- test_findPath.py
+++ test_findPath.py
@@ -78,6 +78,17 @@ class testFindLCA(unittest.TestCase):
         self.assertEqual(findLCA(root, 1, 6), 1)
         self.assertEqual(findLCA(root, 1, 5), 1)
 
+    def testLCAself(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+        self.assertEqual(findLCA(root, 7, 7), 7)
+        self.assertEqual(findLCA(root, 1, 1), 1)
+
     def testLCAfalse(self):
         root = Node(1)
         root.left = Node(2)
